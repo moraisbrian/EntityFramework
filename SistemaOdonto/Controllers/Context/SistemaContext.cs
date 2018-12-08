413c967ea9ce6560f2095dd5124cf5a2ed41ccc7
Brian Leonardo de Morais <morais.brian360@gmail.com>
2018-12-08 18:11:54 -0200

    Aula 10 concluida

---
+++ SistemaOdonto/Controllers/Context/SistemaContext.cs
@@ -0,0 +1,29 @@
+﻿using Controllers.Map;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.Context
+{
+    class SistemaContext : DbContext
+    {
+        public SistemaContext() : base("Server=(local)\\sqlexpress; Database=odonto; Integrated Security=true;")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<SistemaContext>(null);
+            modelBuilder.Configurations.Add(new DentistaMap());
+            modelBuilder.Configurations.Add(new PacienteMap());
+            modelBuilder.Configurations.Add(new ConsultaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SistemaOdonto/Controllers/Map/DentistaMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Controllers.Map
+{
+    class DentistaMap : EntityTypeConfiguration<Dentista>
+    {
+        public DentistaMap()
+        {
+            this.ToTable("Dentista");
+            this.HasKey(d => d.Id);
+            this.Property(d => d.Id).HasColumnName("ID_DENTISTA");
+            this.Property(d => d.Nome).HasColumnName("NOME_DENTISTA");
+            this.Property(d => d.Telefone).HasColumnName("TELEFONE_DENTISTA");
+            this.Property(d => d.Celular).HasColumnName("CELULAR_DENTISTA");
+            this.Property(d => d.CRO).HasColumnName("CRO");
+        }
+    }
+}
